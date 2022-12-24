21af1239886909a6b4371eb7723585541f5399b4
rafiharahap <rafi.harahaps@gmail.com>
2022-12-24 19:26:37 +0700

    edit app.js structure page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 // import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Nav from './components/Nav';
+import Task from './components/Task';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Container>
+       <div className="row">
+        <div className="col-md">
+          <Task />
+        </div>
+      </div> 
+      </Container>
+      
       <h1>Testing Rakamin</h1>
     </div>
   );
