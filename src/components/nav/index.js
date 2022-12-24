32c0341882ec28e82729124224aed2b45a1df472
naufalrafi-hub <rafi.harahaps@gmail.com>
2022-12-25 01:52:08 +0700

    navs done

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,11 +4,12 @@ import './nav.css'
 
 const Nav = () => {
   return (
-        <div className='wrap-nav'>
-          <div className='d-flex ml-20 mt-18 gap-10 align-items-center'>
-            <h1 className='h-1'>Product Roadmap</h1>
+        <div className='wrap-nav px-20 py-18 '>
+          <div className='d-flex gap-10'>
+            <h1 className='h-1 my-auto'>Product Roadmap</h1>
             <Button>+ Add New Group</Button>
           </div>
+          <div className='line-div'></div>
         </div>
   )
 }
